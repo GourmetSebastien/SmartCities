85e7432f074a2c7ac78a3337c1628f8e043231fa
Sébastien Gourmet <sebastien.gourmet@student.hepl.be>
2024-09-20 14:53:17 +0200

    v 0.2.1 add button counter

--- Lesson5.py
+++ Lesson5.py
@@ -4,11 +4,15 @@ import utime
 LED = machine.Pin(16,machine.Pin.OUT)
 BUTTON = machine.Pin(18,machine.Pin.IN)
 
+val = 0
+
 while True:
-    val = BUTTON.value()
-    if val == 1:
-        LED.value(1)
-    else:
-        LED.value(0)
+    if BUTTON.value() == 1:
+        val += 1
+        utime.sleep(1)
 
-
+    elif val == 2 :
+        val = 0
+        utime.sleep(1)
+        
+    LED.value(val)
